8c928c8292b153fa2b03ebdcc195d2ad005633ff
Rtamizharasan <tamilarasan607@gmail.com>
2022-03-29 22:05:49 +0530

    edited

--- src/test/java/org/base/AdactinExcel.java
+++ src/test/java/org/base/AdactinExcel.java
@@ -1,6 +1,21 @@
 package org.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AdactinExcel {
+	
+	public static void main(String[] args) {
+
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+	}
 	
 
 }
